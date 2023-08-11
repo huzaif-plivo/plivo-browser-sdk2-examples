7b91085cf2be589ad8dce6ed5430ba7ad6ca69a1
Mohammed Huzaif <huzaif@plivo.com>
2023-08-11 19:17:40 +0530

    Update customclient.js

--- webApp-plivo/public/js/customclient.js
+++ webApp-plivo/public/js/customclient.js
@@ -10,7 +10,7 @@ let incomingNotificationAlert = null;
 var defaultSettings = {
 	"debug":"INFO",
 	"permOnClick":false,
-	"codecs":[ "PCMU" ],
+	"codecs":[ "PCMU","OPUS" ],
 	"enableIPV6":false,
 	"audioConstraints":{
 	"optional":[ {
