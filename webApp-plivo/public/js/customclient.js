d3824a4529050b36b20836bf8594c4316ca32e05
sanyamjain-plivo <99676817+sanyamjain-plivo@users.noreply.github.com>
2023-09-22 19:48:37 +0530

    Update customclient.js

--- webApp-plivo/public/js/customclient.js
+++ webApp-plivo/public/js/customclient.js
@@ -23,6 +23,7 @@ var defaultSettings = {
 	"enableTracking":true,
 	"closeProtection":false,
 	"maxAverageBitrate":48000,
+	"preDetectOwa":true,
 	"allowMultipleIncomingCalls":false,
 	"dtmfOptions":{sendDtmfType:["outband","inband"]} 
   };
